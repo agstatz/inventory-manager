93d60359563095fcd4cae62e1beaa8c3e1abb408
Ashton <90576442+agstatz@users.noreply.github.com>
2022-03-10 22:18:35 -0500

    Added tables to database, added MySQL database

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # inventory-manager
 An inventory manager application for CS 348 Information Systems 
+
+- Uses MySQL, Python 3.7+
+
+- Make sure to run the following commands locally to have
+- the correct packages installed
+
+- pip install django
+- pip install django-cors-headers
+- pip install djangorestframework
+- pip install mysqlclient
+- pip install wheel
+
+The database is run locally on port 33060. Uses root user whose
+username is root and password is Batwing2020.
--- backend/interface/models.py
+++ backend/interface/models.py
@@ -1,6 +1,41 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    employee_id = models.IntegerField(primary_key=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    phoneNumber = models.CharField(max_length=10)
+    job_title = models.CharField(max_length=30)
+    salary = models.IntegerField()
+
+class Department(models.Model):
+    department_id = models.IntegerField(primary_key=True)
+    department_name = models.CharField(max_length=40)
+
+class Location(models.Model):
+    location_id = models.IntegerField(primary_key=True)
+    street = models.CharField(max_length=40)
+    city = models.CharField(max_length=40)
+    country = models.CharField(max_length=40)
+
+
+class ItemCategory(models.Model):
+    category_id = models.IntegerField(primary_key=True)
+    category_name = models.CharField(max_length=40)
+
+class Item(models.Model):
+    item_id = models.IntegerField(primary_key=True)
+    item_name = models.CharField(max_length=40)
+    quantity = models.IntegerField()
+    weight = models.IntegerField()
+    price = models.FloatField()
+    category_id = models.ForeignKey(ItemCategory, on_delete=models.CASCADE)
+
+class Members(models.Model):
+    member_id = models.IntegerField(primary_key=True)
+
 
 class Interface(models.Model):
     title = models.CharField(max_length=120)
