729e9d6cc9c08b21c97dc90bbbefa6afdf2a8fcb
Ashton <90576442+agstatz@users.noreply.github.com>
2022-03-22 01:11:23 -0400

    Fixed home page formatting

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,9 @@
 import { Text, Heading, Box, Center, Stack, Image,
          Grid, GridItem } from '@chakra-ui/react';
 import { Navbar, SearchWidget } from "../components/";
-import HeaderImage from "../assets/headerimage.jpg";
+import HeaderImageReference from "../assets/headerimage.jpg";
+
+
 
 function Home() {
     return (
@@ -10,9 +12,9 @@ function Home() {
             <Navbar />
             <Box width="100%" bg="brand.300" color="brand.600">
                 <Center>
-                    <Box bg="brand.500" m={[10, 10, 30, 40]} mt={[20, 20, 20, 20]} borderRadius="5px">
-                        <Image width="100%" src={HeaderImage} borderTopRadius="5px" overflow="hidden" my="-10"></Image>
-                        <Stack m={[10, 10, 10, 10]} mt={[20, 20, 20, 20]}>
+                    <Box bg="brand.500" m={[10, 10, 30, 40]} mt={[20, 20, 20, 20]} borderRadius="5px" maxWidth="1020px">
+                        <Image width="100%" src={HeaderImageReference} borderTopRadius="5px" my="-10"></Image>    
+                        <Stack m={[10, 10, 10, 10]} mt={[20, 20, 20, 20]} >
                             <Grid columns={[1, 1, 1, 2]} gap={3} templateColumns='repeat(5, 1fr)'>
                                 <GridItem colSpan={3}>
                                     <Heading py="2">Welcome to Inventory Manager</Heading>
@@ -29,7 +31,9 @@ function Home() {
                                     </Text>
                                 </GridItem>
                                 <GridItem colSpan={2} mx="4">
-                                    <SearchWidget />
+                                    <Center>
+                                        <SearchWidget />
+                                    </Center>
                                 </GridItem>
                             </Grid>
                         </Stack>
